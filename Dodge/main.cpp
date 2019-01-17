11c66f98b64dad46b8cd213370c72a8c15814f6e
Marvin Cirignano <mcirigna@s-128-97-167-140.resnet.ucla.edu>
2019-01-17 10:29:50 -0800

    Initial Commit

---
+++ Dodge/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Dodge
+//
+//  Created by Marvin Cirignano on 1/17/19.
+//  Copyright © 2019 Marvin Cirignano. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
